856f01b5c1066f732c7c1e8f2a637cf5f2ef8563
SatrioBayu <sbayu875@gmail.com>
2022-04-06 02:01:55 +0700

    Initial Commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <h1>First React Component</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Unknown Endpoint So Cannot fetch data");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false);
+        });
+    }, 1000);
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
